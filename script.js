9d0e8d5f56ee5dbac255bca250670bc5a314e00f
Alex W <amwiederman15@gmail.com>
2022-12-11 09:29:32 -0500

    Solved local storage problem and everything seems to work

--- script.js
+++ script.js
@@ -1,58 +1,87 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-  var timeDisplayEl = $('#currentDay');
+$(function () {
+var timeDisplayEl = $('#currentDay');
 var current24HourString = dayjs().format('H');
-var current24Hour = Math.floor(current24HourString)
+// Displaying time as an integer, not a string
+var current24Hour = Math.floor(current24HourString);
 // var current24Hour = 10;
-var saveButton = document.querySelectorAll(".saveBtn")
-// var description = document.querySelectorAll('.description')
+var saveButton = document.querySelectorAll(".saveBtn");
+var description = document.querySelectorAll('.description');
+// var intArray = new Array();
+// var descArray
+
+var descArray = localStorage.getItem('descriptions')
+//Collecting all of the local storage into an array to be displayed initially
+function readDescriptionsLocalStorage() {
+  // Only parsing it if there is something already initialized in the local storage
+  if (descArray) {
+    descArray = JSON.parse(descArray);
+  } else {
+    descArray = [];
+  }
+  return descArray;
+}
+
+// putting the text on the webpage
+function displayDescriptions() {
+for (var i = 0; i < description.length; i++) {
+  
+  if (descArray[i] != undefined) {
+    description[i].value = descArray[i];
+  }
+}
+}
 
 // selecting all slot items to change background color
-var timeSlots = document.querySelectorAll(".time-block")
+var timeSlots = document.querySelectorAll(".time-block");
 
-saveButton.forEach(function(saveButton) {
-  saveButton.addEventListener('click', function(event) {
+// putting text in local storage when hitting any save button
+saveButton.forEach(function (saveButton) {
+  saveButton.addEventListener('click', function (event) {
     var parent = this.parentElement;
     var parentId = parent.id;
-    
-    if (parentId == "hour-9") {
-      for (var i=0; i< parent.childNodes.length; i++) {
-        var checkString = parent.childNodes[i].className
-        if (checkString != undefined){
-        var check = checkString.includes('description')
+    //Going through all of the children to find the description element in order to grab the inputed text
+    for (var i = 0; i < parent.childNodes.length; i++) {
+      var checkString = parent.childNodes[i].className
+      if (checkString != undefined) {
+        var check = checkString.includes('description');
         if (check == true) {
           var textEl = parent.childNodes[i];
-          localStorage.setItem("nineAm",textEl.value)
-          console.log(textEl.value)
-          break;  
-        }
+          
+          if (parentId == "hour-9") {
+            descArray[0] = textEl.value;
+            // localStorage.setItem("nineAm", textEl.value);
+          } else if (parentId == "hour-10") {
+            descArray[1] = textEl.value;
+            // localStorage.setItem("tenAm", textEl.value);
+          } else if (parentId == "hour-11") {
+            descArray[2] = textEl.value;
+            // localStorage.setItem("elevenAm", textEl.value);
+          } else if (parentId == "hour-12") {
+            descArray[3] = textEl.value;
+            // localStorage.setItem("twelvePm", textEl.value);
+          } else if (parentId == "hour-13") {
+            descArray[4] = textEl.value;
+            // localStorage.setItem("onePm", textEl.value);
+          } else if (parentId == "hour-14") {
+            descArray[5] = textEl.value;
+            // localStorage.setItem("twoPm", textEl.value);
+          } else if (parentId == "hour-15") {
+            descArray[6] = textEl.value;
+            // localStorage.setItem("threePm", textEl.value);
+          } else if (parentId == "hour-16") {
+            descArray[7] = textEl.value;
+            // localStorage.setItem("fourPm", textEl.value);
+          } else if (parentId == "hour-17") {
+            descArray[8] = textEl.value;
+            // localStorage.setItem("fivePm", textEl.value);
+          }
+          localStorage.setItem("descriptions", JSON.stringify(descArray));
+          break;
         }
       }
-
-    console.log(textEl.textContent)
     }
-   
-     
   });
 });
-
-
-
-
 
 // Putting 24 hour clock into a variable
 function timeDisplay() {
@@ -61,40 +90,42 @@ function timeDisplay() {
 }
 
 //Changing Background Color of slots
-for (i=9; i <18; i++) {
+for (i = 9; i < 18; i++) {
   if (current24Hour == i) {
-    if (timeSlots[i-9].classList.contains("past")) {
-    timeSlots[i-9].classList.remove("past");
+    if (timeSlots[i - 9].classList.contains("past")) {
+      timeSlots[i - 9].classList.remove("past");
     }
-    if (timeSlots[i-9].classList.contains("future")) {
-      timeSlots[i-9].classList.remove("future");
-      }
-      timeSlots[i-9].classList.add("present");
+    if (timeSlots[i - 9].classList.contains("future")) {
+      timeSlots[i - 9].classList.remove("future");
+    }
+    timeSlots[i - 9].classList.add("present");
   } else if (current24Hour > i) {
-    if (timeSlots[i-9].classList.contains("present")) {
-    timeSlots[i-9].classList.remove("present");
+    if (timeSlots[i - 9].classList.contains("present")) {
+      timeSlots[i - 9].classList.remove("present");
     }
-    if (timeSlots[i-9].classList.contains("future")) {
-      timeSlots[i-9].classList.remove("future");
-      }
-      timeSlots[i-9].classList.add("past");
+    if (timeSlots[i - 9].classList.contains("future")) {
+      timeSlots[i - 9].classList.remove("future");
+    }
+    timeSlots[i - 9].classList.add("past");
   } else if (current24Hour < i) {
-    if (timeSlots[i-9].classList.contains("present")) {
-    timeSlots[i-9].classList.remove("present");
+    if (timeSlots[i - 9].classList.contains("present")) {
+      timeSlots[i - 9].classList.remove("present");
     }
-    if (timeSlots[i-9].classList.contains("past")) {
-      timeSlots[i-9].classList.remove("past");
-      }
-      timeSlots[i-9].classList.add("future");
+    if (timeSlots[i - 9].classList.contains("past")) {
+      timeSlots[i - 9].classList.remove("past");
+    }
+    timeSlots[i - 9].classList.add("future");
   }
-  
 }
 
+// Initializing the descriptions to display first if not empty
+readDescriptionsLocalStorage()
 
+displayDescriptions()
 // Initializing time to display
 timeDisplay();
 //Setting display to update every 1000
-setInterval(timeDisplay,1000);
+setInterval(timeDisplay, 1000);
 
-// });
+});
 
